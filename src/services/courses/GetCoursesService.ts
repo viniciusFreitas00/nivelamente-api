c33885c788f009e5438713efcf5865b72f22d5f9
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2021-09-22 20:42:11 -0300

    fix get courses response :hammer:

--- src/services/courses/GetCoursesService.ts
+++ src/services/courses/GetCoursesService.ts
@@ -10,12 +10,12 @@ async function GetCoursesServices({
 }: Request): Promise<Courses[] | Courses | null> {
   if (courseID == 'All') {
     let courses = await database.query<Courses[]>(
-      `SELECT ID,USER_ID, DESCRIPTION, CATEGORY, STATUS, CREATED_AT,UPDATED_AT, CONCAT('https://recipes-share.s3.sa-east-1.amazonaws.com/', IMAGE) as IMAGE FROM COURSES WHERE STATUS = 'published'`,
+      `SELECT ID,USER_ID, TITLE, DESCRIPTION, CATEGORY, STATUS, CREATED_AT,UPDATED_AT, CONCAT('https://recipes-share.s3.sa-east-1.amazonaws.com/', IMAGE) as IMAGE FROM COURSES WHERE STATUS = 'published'`,
     );
     return courses;
   } else {
     let courses = await database.oneOrNone<Courses>(
-      `SELECT ID,USER_ID, DESCRIPTION, CATEGORY, STATUS, CREATED_AT,UPDATED_AT, CONCAT('https://recipes-share.s3.sa-east-1.amazonaws.com/', IMAGE) as IMAGE FROM COURSES WHERE ID = $[courseID] AND STATUS = 'published'`,
+      `SELECT ID,USER_ID, TITLE, DESCRIPTION, CATEGORY, STATUS, CREATED_AT,UPDATED_AT, CONCAT('https://recipes-share.s3.sa-east-1.amazonaws.com/', IMAGE) as IMAGE FROM COURSES WHERE ID = $[courseID] AND STATUS = 'published'`,
       { courseID },
     );
     return courses;
